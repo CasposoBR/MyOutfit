1cd412da0cb0f9def318ba6243df112e49af2c28
Ryan <ryanguilherme.1708@hotmail.com>
2025-04-08 17:51:39 -0300

    resolvido problema que ao iniciar, não aparecia nada, mudança no fun AuthScreen dentro da MainActivity(necessitava puxar o firebaseAuth e importar)

--- app/src/main/java/com/example/myoutfit/MainActivity.kt
+++ app/src/main/java/com/example/myoutfit/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import com.example.myoutfit.ui.theme.MyOutfitTheme
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,5 +23,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AuthScreen() {
-    // Implemente a tela aqui
-}
+    val auth = remember { FirebaseAuth.getInstance() }
+    LoginScreen(auth = auth)
+
+    }
