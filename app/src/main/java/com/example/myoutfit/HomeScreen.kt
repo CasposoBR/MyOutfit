6b1ffd66239a7455b56cd551204b48e2518cf024
Ryan <ryanguilherme.1708@hotmail.com>
2025-04-15 02:22:05 -0300

    atualizações do layout do HomeScreen, adicionado mais categorias, como category winter, summer e workout.

--- app/src/main/java/com/example/myoutfit/HomeScreen.kt
+++ app/src/main/java/com/example/myoutfit/HomeScreen.kt
@@ -1,3 +1,4 @@
+// TODO: Organizar categorias corretamente
 package com.example.myoutfit
 
 import android.content.Intent
@@ -9,6 +10,8 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
@@ -24,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import androidx.compose.ui.layout.ContentScale
 
 @Composable
 fun MyOutfitHomeScreen() {
@@ -52,6 +56,11 @@ fun MyOutfitHomeScreen() {
         }
     }
 }
+data class Product(val imageUrl: String, val title: String, val link: String, val category: CategoryType)
+
+enum class CategoryType {
+    SUMMER, WINTER, TREND, WORKOUT
+}
 
 enum class BottomNavItem(val label: String, val icon: ImageVector) {
     HOME("Início", Icons.Default.Home),
@@ -61,52 +70,162 @@ enum class BottomNavItem(val label: String, val icon: ImageVector) {
 
 @Composable
 fun HomeContent() {
-    val trends = listOf(
-        Triple(
-            "https://img.ltwebstatic.com/images3_pi/2023/11/14/0b/1699949722745bdb33179b63690fd02cfdd61fd1b4_thumbnail_720x.jpg",
+    val products = listOf(
+        Product(
+            "https://img.ltwebstatic.com/images3_pi/2024/08/28/b7/1724811269f1b9f1b91b4f0b2dc34b7f2aca60c8a5.webp",
             "Jaqueta Preta PU",
-            "https://br.shein.com/SWAVVY-Men-s-Fashionable-Daily-Casual-PU-Leather-Fitted-Long-Sleeve-Men-Jacket-Urban-Black-Jacket-For-Friends-Husband-Boyfriend-Gifts-p-41641968.html"
+            "https://br.shein.com/SWAVVY-Men-s-Fashionable-Daily-Casual-PU-Leather-Fitted-Long-Sleeve-Men-Jacket-Urban-Black-Jacket-For-Friends-Husband-Boyfriend-Gifts-p-41641968.html?mallCode=1&imgRatio=3-4",
+            CategoryType.WINTER
         ),
-        Triple(
-            "https://img.ltwebstatic.com/images3_pi/2023/09/28/3e/1695897567ae01b13e7a184b3b2adff6c81a850517_thumbnail_720x.jpg",
+        Product(
+            "https://img.ltwebstatic.com/images3_spmp/2024/10/03/d3/172796272339daa9f54e33fe4bff473d187a24f118_thumbnail_336x.webp",
             "Camiseta Oversized",
-            "https://br.shein.com/Men-T-Shirts-p-25143085.html"
+            "https://br.shein.com/Men-T-Shirts-p-25143085.html",
+            CategoryType.TREND
         ),
-        Triple(
-            "https://img.ltwebstatic.com/images3_pi/2022/09/27/16642676311e0c5aaf104bf0474c2b47654c4a0276_thumbnail_720x.jpg",
+        Product(
+            "https://img.ltwebstatic.com/images3_spmp/2023/06/12/16865104766b5b3b261d29d57196290c691452416f.webp",
             "Jaqueta Jeans",
-            "https://br.shein.com/Men-Denim-Jackets-p-14722960.html"
+            "https://br.shein.com/Men-Denim-Jackets-p-14722960.html",
+            CategoryType.WINTER
         ),
-        Triple(
-            "https://img.ltwebstatic.com/images3_pi/2022/08/10/1660109833f7cb7542d3b1724a67c23309fef9b56f_thumbnail_720x.jpg",
+        Product(
+            "https://img.ltwebstatic.com/images3_pi/2024/03/06/59/17097300463527cb3179a7e75552444fd907ba6b23.webp",
             "Calça Alfaiataria",
-            "https://br.shein.com/Manfinity-Homme-Men-s-Casual-Solid-Color-Slant-Pocket-Drawstring-Pants-Straight-Leg-Long-Slacks-Plain-Going-Out-p-3115655.html"
-        )
+            "https://br.shein.com/Manfinity-Homme-Men-s-Casual-Solid-Color-Slant-Pocket-Drawstring-Pants-Straight-Leg-Long-Slacks-Plain-Going-Out-p-3115655.html",
+            CategoryType.TREND
+        ),
+        Product(
+            "https://img.ltwebstatic.com/images3_spmp/2023/09/02/9a/1693662275a6500efb50040557607c45a67aa3b08c.webp",
+            "Regata Canelada",
+            "https://br.shein.com/Men-Tank-Tops-p-22602261.html",
+            CategoryType.SUMMER
+        ),
+        Product(
+            "https://img.ltwebstatic.com/images3_spmp/2025/02/19/4c/17399750382ff6a6719daecabbd80697b67f8fc201_square.webp",
+            "Camisa Dry Fit",
+            "https://br.shein.com/Men-s-Short-Sleeve-Dry-Fit-Gym-Fitness-T-Shirt-Lisa-Plus-Size-Zoo-p-48405365.html",
+            CategoryType.WORKOUT
+        ),
+        Product(
+            "https://img.ltwebstatic.com/images3_spmp/2024/08/17/97/1723836643d25ffdb991394f79b4bce2ac1d8f3419_thumbnail_336x.webp",
+            "Camisa Oversized Academia Fit",
+            "https://br.shein.com/Oversized-Shirt-Gym-Fashion-Fit-Bodybuilding-p-40991466.html",
+            CategoryType.WORKOUT
+        ),
+        Product(
+            "https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/4832058447_1029674/00ABECC87B4CB31F239FC5CE93594C71.webp",
+            "Slim Fitness SLM Camiseta UV Masculina Proteção Solar",
+            "https://br.shein.com/Slim-Fitness-SLM-Men-T-Shirts-Tanks-p-12893370.html",
+            CategoryType.WORKOUT
+        ),
+        Product(
+            "https://img.ltwebstatic.com/images3_spmp/2024/12/19/80/17346196374ea253869f05097f48bd52ce0311635f.webp",
+            "Camisa de Linho Manga curta Social",
+            "br.shein.com/Linen-Shirt-Short-Sleeve-Formal-Summer-Fresh-Italian-Collar-Elegant-In-Visco-Linen-p-50917714.html",
+            CategoryType.SUMMER
+        ),
+        Product(
+            "https://img.ltwebstatic.com/images3_spmp/2024/01/30/1b/1706556936ae19911a343e9b7ba6a9732a033a3107.webp",
+            "Calça Masculina Cargo Sarja Skatista",
+            "https://br.shein.com/Men-Jeans-p-30021128.html",
+            CategoryType.TREND
+        ),
+
+        Product(
+            "https://img.ltwebstatic.com/images3_spmp/2024/02/02/28/17068460474610b8f45f7cdfb741129b0b4b5abaa4.webp",
+            "Bermuda Tecido Sarja",
+            "br.shein.com/Men-Shorts-p-13972715.html",
+            CategoryType.SUMMER
+        ),
+
+
+
+
     )
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text("Estilos Populares", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-        Spacer(Modifier.height(8.dp))
-        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            listOf("Star Boy", "Old Money", "Skatista").forEach { style ->
-                AssistChip(onClick = { /* navigate to style */ }, label = { Text(style) })
-            }
-        }
-        Spacer(Modifier.height(24.dp))
+    val summerProducts = products.filter { it.category == CategoryType.SUMMER }
+    val winterProducts = products.filter { it.category == CategoryType.WINTER }
+    val trendProducts = products.filter { it.category == CategoryType.TREND }
+    val workoutProducts = products.filter { it.category == CategoryType.WORKOUT }
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+            .padding(16.dp)
+    ) {
+        // Seção para o Verão
         Text("Tendências", fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
         Spacer(Modifier.height(8.dp))
         LazyRow(
             horizontalArrangement = Arrangement.spacedBy(16.dp),
             contentPadding = PaddingValues(horizontal = 16.dp)
         ) {
-            items(trends) { (image, title, link) ->
+            items(trendProducts) { product ->
                 TrendCard(
-                    imageUrl = image,
-                    title = title,
-                    link = link
+                    imageUrl = product.imageUrl,
+                    title = product.title,
+                    link = product.link
                 )
             }
         }
+
+        Spacer(Modifier.height(24.dp))
+
+        // Seção para o Inverno
+        Text("Para o Verão", fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
+        Spacer(Modifier.height(8.dp))
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
+        ) {
+            items(summerProducts) { product ->
+                TrendCard(
+                    imageUrl = product.imageUrl,
+                    title = product.title,
+                    link = product.link
+                )
+            }
+        }
+
+        Spacer(Modifier.height(24.dp))
+
+        // Seção de Tendências
+        Text("Para o Inverno", fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
+        Spacer(Modifier.height(8.dp))
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
+        ) {
+            items(winterProducts) { product ->
+                TrendCard(
+                    imageUrl = product.imageUrl,
+                    title = product.title,
+                    link = product.link
+                )
+            }
+        }
+        Spacer(Modifier.height(24.dp))
+
+        // Seção de Tendências
+        Text("Para o seu Treino", fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
+        Spacer(Modifier.height(8.dp))
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
+        ) {
+            items(workoutProducts) { product ->
+                TrendCard(
+                    imageUrl = product.imageUrl,
+                    title = product.title,
+                    link = product.link
+                )
+            }
+        }
+
+
     }
 }
 
@@ -130,9 +249,11 @@ fun TrendCard(imageUrl: String, title: String, link: String) {
             AsyncImage(
                 model = imageUrl,
                 contentDescription = title,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .width(160.dp)
                     .height(160.dp)
+
             )
             Text(
                 text = title,
